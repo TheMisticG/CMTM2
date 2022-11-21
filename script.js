71c40d50797a84f27f326894bc22060238e32703
TheMisticG <118356120+TheMisticG@users.noreply.github.com>
2022-11-21 12:12:17 +0100

    Update

---
+++ script.js
@@ -0,0 +1,16 @@
+function welcomeFunc() {
+    window.alert("Welcome to my Official Website\nby MisticG");
+};
+
+function discordinfo() {
+    document.getElementById('discordinfo').classList.add("blueopen");
+    window.alert("You can text me on discord using the following name:\nMisticG#4211");
+};
+
+function founderimgclicked() {
+    document.getElementById('creatorimgclicked').classList.toggle("founderimgclicked");
+}
+
+function beginnerclick() {
+    document.getElementsById("beginner").classList.toggle("beginner1click");
+}
