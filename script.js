b5ab2595c305581ec58ece174e1a2e01ad3d6bb5
TheMisticG <118356120+TheMisticG@users.noreply.github.com>
2022-11-21 12:28:27 +0100

    Update

--- script.js
+++ script.js
@@ -3,14 +3,5 @@ function welcomeFunc() {
 };
 
 function discordinfo() {
-    document.getElementById('discordinfo').classList.add("blueopen");
     window.alert("You can text me on discord using the following name:\nMisticG#4211");
 };
-
-function founderimgclicked() {
-    document.getElementById('creatorimgclicked').classList.toggle("founderimgclicked");
-}
-
-function beginnerclick() {
-    document.getElementsById("beginner").classList.toggle("beginner1click");
-}
