ba85d80a08b8f3145ee7d74cfd7b8c2e189db882
TheMisticG <118356120+TheMisticG@users.noreply.github.com>
2022-11-22 08:43:36 +0100

    Update

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function welcomeFunc() {
 function discordinfo() {
     window.alert("You can text me on discord using the following name:\nMisticG#4211");
 };
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = checkTime(h)
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+}
+  
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
