9a8a90ea792e52157661256ee034f4e746a8d995
TheMisticG <118356120+TheMisticG@users.noreply.github.com>
2023-01-13 09:41:16 +0100

    Update

--- NotUsedJS.js
+++ NotUsedJS.js
@@ -1,29 +1,26 @@
-# Not used JS here of official "script.js"
-
-```javascript
 function welcomeFunc() {
-    window.alert("Welcome to my Official Website\nby MisticG");
+	window.alert("Welcome to my Official Website\nby MisticG");
 };
 
 function discordinfo() {
-    window.alert("You can text me on discord using the following name:\nMisticG#4211");
+	window.alert("You can text me on discord using the following name:\nMisticG#4211");
 };
 
 function startTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    h = checkTime(h)
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML =  h + ":" + m;
-    setTimeout(startTime, 1000);
+	const today = new Date();
+	let h = today.getHours();
+	let m = today.getMinutes();
+	let s = today.getSeconds();
+	h = checkTime(h)
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('clock').innerHTML =  h + ":" + m;
+	setTimeout(startTime, 1000);
 }
   
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	return i;
 }
 
 const countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
@@ -51,8 +48,8 @@ const countDown = setInterval(function() {
 }, 500);
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+	if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	return i;
 };
 
 setInterval(function(){
@@ -64,4 +61,3 @@ setInterval(function(){
 		element.innerHTML = "+39 3281492299";
 	}
 }, 500);
-```
